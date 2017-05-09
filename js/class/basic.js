46b930dd52d3b451433568e556d637df0c1cf4ca
Rinwasyu <maikaimi@yahoo.co.jp>
2017-05-09 20:17:03 +0900

    Create basic class

---
+++ js/class/basic.js
@@ -0,0 +1,15 @@
+/*
+	Class_name  : Basic
+	Description : Root of all Classes
+*/
+
+class Basic {
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+	}
+	setPosition(x,y) {
+		this.x = x;
+		this.y = y;
+	}
+}
