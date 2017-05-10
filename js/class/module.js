b753c2ba8365c72a12d2ae1ca8595add8e1910fc
Rinwasyu <maikaimi@yahoo.co.jp>
2017-05-10 16:21:28 +0900

    Update module class

--- js/class/module.js
+++ js/class/module.js
@@ -4,5 +4,24 @@
 */
 
 class Module extends Basic {
-  
+	init() {
+		this.insideColor = "gray";
+		this.borderColor = "black";
+		this.size = 0;
+		return this;
+	}
+	setSize(size) {
+		this.size = size;
+		return this;
+	}
+	draw() {
+		x.fillStyle = this.insideColor;
+		x.strokeStyle = this.borderColor;
+		x.lineWidth = this.size * 0.3;
+		x.beginPath();
+		x.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+		x.fill();
+		x.stroke();
+		return this;
+	}
 }
